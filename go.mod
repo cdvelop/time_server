627a5e745aa138cef5ffbcbfef975cc2e9c8307f
Cesar Solis <44058491+cdvelop@users.noreply.github.com>
2023-12-04 04:38:53 -0300

    update module: model

--- go.mod
+++ go.mod
@@ -2,12 +2,12 @@ module github.com/cdvelop/timeserver
 
 go 1.20
 
-require github.com/cdvelop/timetools v0.0.24
-
 require (
-	github.com/cdvelop/model v0.0.75 // indirect
-	github.com/cdvelop/strings v0.0.7 // indirect
+	github.com/cdvelop/strings v0.0.7
+	github.com/cdvelop/timetools v0.0.25
 )
+
+require github.com/cdvelop/model v0.0.76 // indirect
 
 replace github.com/cdvelop/model => ../model
 
--- today.go
+++ today.go
@@ -2,6 +2,8 @@ package timeserver
 
 import (
 	"time"
+
+	"github.com/cdvelop/strings"
 )
 
 // layout ej: 2006-01-02 15:04
@@ -12,7 +14,14 @@ func (t timeServer) ToDay(layout string) string {
 	}
 
 	if t.current_date != "" {
-		return t.current_date
+		var hour string
+		if layout == "2006-01-02 15:04:05" && strings.Contains(t.current_date, ":") == 0 {
+			ahora := time.Now()
+			hour = " " + ahora.Format("15:04:05")
+		}
+
+		// 2006-01-02 15:04:05
+		return t.current_date + hour
 	}
 
 	return time.Now().Format(layout)
