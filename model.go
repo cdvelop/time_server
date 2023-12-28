2176ed7c38364a72abeba079f8b508087714356f
Cesar Solis <44058491+cdvelop@users.noreply.github.com>
2023-12-28 18:40:36 -0300

    update module: timetools

--- model.go
+++ model.go
@@ -2,4 +2,5 @@ package timeserver
 
 type timeServer struct {
 	fake_date string // ej "2023-08-29"
+
 }
--- today.go
+++ today.go
@@ -3,17 +3,15 @@ package timeserver
 import (
 	"time"
 
+	"github.com/cdvelop/model"
 	"github.com/cdvelop/timetools"
 )
 
-func (t timeServer) DateToDay() string {
-
-	date, _ := timetools.DateToDayHour(time.Now().Format("2006-01-02 15:04:05"), t.fake_date)
-
+func (t timeServer) DateToDay(df *model.DateFormat) string {
+	date, _ := timetools.DateToDayHour(time.Now().Format("2006-01-02 15:04:05"), t.fake_date, df)
 	return date
 }
 
-func (t timeServer) DateToDayHour(seconds ...bool) (date, hour string) {
-
-	return timetools.DateToDayHour(time.Now().Format("2006-01-02 15:04:05"), t.fake_date, seconds...)
+func (t timeServer) DateToDayHour(df *model.DateFormat) (date, hour string) {
+	return timetools.DateToDayHour(time.Now().Format("2006-01-02 15:04:05"), t.fake_date, df)
 }
