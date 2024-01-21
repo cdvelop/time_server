72f18fa944c02c80b67390842e78494189398227
Cesar Solis <44058491+cdvelop@users.noreply.github.com>
2024-01-21 13:16:31 -0300

    update module: timetools refactor DateToDay DateToDayHour

--- today.go
+++ today.go
@@ -6,11 +6,11 @@ import (
 	"github.com/cdvelop/timetools"
 )
 
-func (t timeServer) DateToDay(df timetools.DateFormatAdapter) string {
-	date, _ := timetools.DateToDayHour(time.Now().Format("2006-01-02 15:04:05"), t.fake_date, df)
+func (t timeServer) DateToDay(dateFormatStructPointer any) string {
+	date, _ := timetools.DateToDayHour(time.Now().Format("2006-01-02 15:04:05"), t.fake_date, dateFormatStructPointer)
 	return date
 }
 
-func (t timeServer) DateToDayHour(df timetools.DateFormatAdapter) (date, hour string) {
-	return timetools.DateToDayHour(time.Now().Format("2006-01-02 15:04:05"), t.fake_date, df)
+func (t timeServer) DateToDayHour(dateFormatStructPointer any) (date, hour string) {
+	return timetools.DateToDayHour(time.Now().Format("2006-01-02 15:04:05"), t.fake_date, dateFormatStructPointer)
 }
