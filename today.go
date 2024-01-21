59ad42d8ede78879d7b424615b0cfc059aa34293
Cesar Solis <44058491+cdvelop@users.noreply.github.com>
2024-01-21 12:41:38 -0300

    remove model dependencie

--- today.go
+++ today.go
@@ -3,15 +3,14 @@ package timeserver
 import (
 	"time"
 
-	"github.com/cdvelop/model"
 	"github.com/cdvelop/timetools"
 )
 
-func (t timeServer) DateToDay(df *model.DateFormat) string {
+func (t timeServer) DateToDay(df timetools.DateFormatAdapter) string {
 	date, _ := timetools.DateToDayHour(time.Now().Format("2006-01-02 15:04:05"), t.fake_date, df)
 	return date
 }
 
-func (t timeServer) DateToDayHour(df *model.DateFormat) (date, hour string) {
+func (t timeServer) DateToDayHour(df timetools.DateFormatAdapter) (date, hour string) {
 	return timetools.DateToDayHour(time.Now().Format("2006-01-02 15:04:05"), t.fake_date, df)
 }
